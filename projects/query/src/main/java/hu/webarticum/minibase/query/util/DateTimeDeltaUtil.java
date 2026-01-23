6916660311787c966adf4b8dc497c5338b68c29c
Dávid Horváth <horvath@webarticum.hu>
2026-01-23 22:01:54 +0100

    Add some additional tests for DateTimeDeltaUtil

--- projects/query/src/main/java/hu/webarticum/minibase/query/util/DateTimeDeltaUtil.java
+++ projects/query/src/main/java/hu/webarticum/minibase/query/util/DateTimeDeltaUtil.java
@@ -5,7 +5,6 @@ import java.math.RoundingMode;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.Period;
 import java.time.ZoneOffset;
 import java.time.temporal.Temporal;
