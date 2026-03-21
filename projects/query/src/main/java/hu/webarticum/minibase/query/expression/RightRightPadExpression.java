d58c4b2e3e8a77a8654cb3bbf50f7f2b6ee72eea
Dávid Horváth <horvath@webarticum.hu>
2026-03-22 00:12:16 +0100

    Ads support for BitString and ByteString in more string functions

--- projects/query/src/main/java/hu/webarticum/minibase/query/expression/RightRightPadExpression.java
+++ projects/query/src/main/java/hu/webarticum/minibase/query/expression/RightRightPadExpression.java
@@ -2,15 +2,16 @@ package hu.webarticum.minibase.query.expression;
 
 import java.util.Optional;
 
+import hu.webarticum.minibase.query.util.BitStringUtil;
+import hu.webarticum.minibase.query.util.ByteStringUtil;
 import hu.webarticum.minibase.query.util.NumberUtil;
 import hu.webarticum.minibase.query.util.StringUtil;
+import hu.webarticum.miniconnect.lang.BitString;
+import hu.webarticum.miniconnect.lang.ByteString;
 import hu.webarticum.miniconnect.lang.ImmutableList;
 import hu.webarticum.miniconnect.lang.ImmutableMap;
 
 public class RightRightPadExpression implements Expression {
-
-    private static final String DEFAULT_PAD_STRING = " ";
-
 
     private final Expression inputExpression;
 
@@ -50,12 +51,22 @@ public class RightRightPadExpression implements Expression {
 
     @Override
     public Optional<Class<?>> type() {
-        return Optional.of(String.class);
+        Class<?> inputType = inputExpression.type().orElse(null);
+        if (inputType == null || inputType == ByteString.class || inputType == BitString.class) {
+            return Optional.ofNullable(inputType);
+        } else {
+            return Optional.of(String.class);
+        }
     }
 
     @Override
     public Class<?> type(ImmutableMap<Parameter, Class<?>> values) {
-        return String.class;
+        Class<?> inputType = inputExpression.type(values);
+        if (inputType == ByteString.class || inputType == BitString.class) {
+            return inputType;
+        } else {
+            return String.class;
+        }
     }
 
     @Override
@@ -86,31 +97,76 @@ public class RightRightPadExpression implements Expression {
             return null;
         }
 
-        String padString = DEFAULT_PAD_STRING;
+        Object padValue = null;
         if (padStringExpression.isPresent()) {
-            Object padStringValue = padStringExpression.get().evaluate(values);
-            if (padStringValue == null) {
+            padValue = padStringExpression.get().evaluate(values);
+            if (padValue == null) {
                 return  null;
             }
-            padString = StringUtil.stringify(padStringValue);
         }
 
-        String inputString = StringUtil.stringify(inputValue);
-        int inputLength = inputString.length();
         int length = NumberUtil.asInt(lengthValue);
+        if (inputValue instanceof ByteString) {
+            return operate((ByteString) inputValue, length, ByteStringUtil.byteStringify(padValue));
+        } else if (inputValue instanceof BitString) {
+            return operate((BitString) inputValue, length, BitStringUtil.bitStringify(padValue));
+        } else {
+            return operate(StringUtil.stringify(inputValue), length, StringUtil.stringify(padValue));
+        }
+    }
+
+    private String operate(String input, int length, String pad) {
+        String effectivePad = pad != null ? pad : " ";
+        int inputLength = input.length();
         int padLength = Math.max(0, length - inputLength);
-        int padStringLength = padString.length();
+        int padStringLength = effectivePad.length();
         int padRepeats = padLength / padStringLength;
         int padFraction = padLength % padStringLength;
-
-        StringBuilder resultBuilder = new StringBuilder(inputString);
+        StringBuilder resultBuilder = new StringBuilder();
+        resultBuilder.append(input);
         if (padFraction > 0) {
-            resultBuilder.append(padString.substring(padStringLength - padFraction));
+            resultBuilder.append(effectivePad.substring(padStringLength - padFraction));
         }
         for (int i = 0; i < padRepeats; i++) {
-            resultBuilder.append(padString);
+            resultBuilder.append(effectivePad);
         }
         return resultBuilder.toString();
+    }
+
+    private ByteString operate(ByteString input, int length, ByteString pad) {
+        ByteString effectivePad = pad != null ? pad : ByteString.ofByte(0);
+        int inputLength = input.length();
+        int padLength = Math.max(0, length - inputLength);
+        int padStringLength = effectivePad.length();
+        int padRepeats = padLength / padStringLength;
+        int padFraction = padLength % padStringLength;
+        ByteString.Builder resultBuilder = ByteString.builder();
+        resultBuilder.append(input);
+        if (padFraction > 0) {
+            resultBuilder.append(effectivePad.substring(padStringLength - padFraction));
+        }
+        for (int i = 0; i < padRepeats; i++) {
+            resultBuilder.append(effectivePad);
+        }
+        return resultBuilder.build();
+    }
+
+    private BitString operate(BitString input, int length, BitString pad) {
+        BitString effectivePad = pad != null ? pad : BitString.of("0");
+        int inputLength = input.length();
+        int padLength = Math.max(0, length - inputLength);
+        int padStringLength = effectivePad.length();
+        int padRepeats = padLength / padStringLength;
+        int padFraction = padLength % padStringLength;
+        BitString.Builder resultBuilder = BitString.builder();
+        resultBuilder.append(input);
+        if (padFraction > 0) {
+            resultBuilder.append(effectivePad.substring(padStringLength - padFraction));
+        }
+        for (int i = 0; i < padRepeats; i++) {
+            resultBuilder.append(effectivePad);
+        }
+        return resultBuilder.build();
     }
 
     @Override
