18fe884da2a8be3241a008c2e699b64e0053b12c
Dávid Horváth <horvath@webarticum.hu>
2026-03-24 12:05:27 +0100

    Add support for the BIT and BINARY types in the REVERSE expression

--- projects/query/src/main/java/hu/webarticum/minibase/query/expression/ReverseExpression.java
+++ projects/query/src/main/java/hu/webarticum/minibase/query/expression/ReverseExpression.java
@@ -3,6 +3,8 @@ package hu.webarticum.minibase.query.expression;
 import java.util.Optional;
 
 import hu.webarticum.minibase.query.util.StringUtil;
+import hu.webarticum.miniconnect.lang.BitString;
+import hu.webarticum.miniconnect.lang.ByteString;
 import hu.webarticum.miniconnect.lang.ImmutableList;
 import hu.webarticum.miniconnect.lang.ImmutableMap;
 
@@ -27,12 +29,28 @@ public class ReverseExpression implements Expression {
 
     @Override
     public Optional<Class<?>> type() {
-        return Optional.of(String.class);
+        Class<?> subType = subExpression.type().orElse(null);
+        if (subType == ByteString.class) {
+            return Optional.of(ByteString.class);
+        } else if (subType == BitString.class) {
+            return Optional.of(BitString.class);
+        } else if (subType != null) {
+            return Optional.of(String.class);
+        } else {
+            return Optional.empty();
+        }
     }
 
     @Override
     public Class<?> type(ImmutableMap<Parameter, Class<?>> values) {
-        return String.class;
+        Class<?> subType = subExpression.type(values);
+        if (subType == ByteString.class) {
+            return ByteString.class;
+        } else if (subType == BitString.class) {
+            return BitString.class;
+        } else {
+            return String.class;
+        }
     }
 
     @Override
@@ -52,7 +70,13 @@ public class ReverseExpression implements Expression {
             return null;
         }
 
-        return new StringBuilder(StringUtil.stringify(value)).reverse().toString();
+        if (value instanceof ByteString) {
+            return ((ByteString) value).reverse();
+        } else if (value instanceof BitString) {
+            return ((BitString) value).reverse();
+        } else {
+            return new StringBuilder(StringUtil.stringify(value)).reverse().toString();
+        }
     }
 
     @Override
